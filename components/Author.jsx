07c13be05a13acb9443c45c12159e4c1c68f2eb3
Akudbilla Daniel <akuddev@gmail.com>
2021-11-30 13:03:21 +0000

    added author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-2 -top-14">
+        <Image
+          alt={author.name}
+          width="100px"
+          height="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="text-white mt-4 my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   );
 };
