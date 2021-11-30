d64694814a4a2baa8b71804253d9cf15d1a055fa
Akudbilla Daniel <akuddev@gmail.com>
2021-11-30 20:10:04 +0000

    added category posts feature

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
 import { PostCard, PostWidget, Categories } from "../components/index";
 import { getPosts } from "../services/index";
 import { FeaturedPosts } from "../sections/index";
